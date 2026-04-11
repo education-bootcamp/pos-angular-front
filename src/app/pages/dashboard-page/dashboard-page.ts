ed613a8cdd3bc1e02ce21822f151a0874f7ca070
hasika-sandaruwan <hasikasandaruwan.info@gmail.com>
2026-04-11 22:07:44 +0530

    nav bar completed

--- src/app/pages/dashboard-page/dashboard-page.ts
+++ src/app/pages/dashboard-page/dashboard-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { DashboardMainHeader } from './inner-pages/core/dashboard-main-header/dashboard-main-header';
 import { DashboardMainFooter } from './inner-pages/core/dashboard-main-footer/dashboard-main-footer';
 import { TabBar } from '../../dto/TabBar';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [DashboardMainHeader, DashboardMainFooter],
+  imports: [DashboardMainHeader, DashboardMainFooter, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.scss',
 })
