42152159a6366bc7846f6fd5f1708d858695980d
education-bootcamp <education.seekerscloud@gmail.com>
2026-06-09 21:17:48 +0530

    init

--- src/app/pages/auth/login-page/login-page.ts
+++ src/app/pages/auth/login-page/login-page.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatLabel } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [MatFormFieldModule, MatLabel, MatInputModule, ReactiveFormsModule, MatButtonModule, RouterLink],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
-export class LoginPage {}
+export class LoginPage {
+
+  loginForm= new FormGroup({
+    email: new FormControl('',Validators.required),
+    password: new FormControl('',Validators.required)
+  })
+
+  execute(){
+    console.log(this.loginForm);
+    
+  }
+
+
+}
--- src/app/pages/auth/register-page/register-page.ts
+++ src/app/pages/auth/register-page/register-page.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatLabel } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [MatFormFieldModule, MatLabel, MatInputModule, ReactiveFormsModule, MatButtonModule, RouterLink],
   templateUrl: './register-page.html',
   styleUrl: './register-page.scss',
 })
-export class RegisterPage {}
+export class RegisterPage {
+  loginForm= new FormGroup({
+    email: new FormControl('',Validators.required),
+    password: new FormControl('',Validators.required)
+  })
+
+  execute(){
+    console.log(this.loginForm);
+    
+  }
+
+
+}
