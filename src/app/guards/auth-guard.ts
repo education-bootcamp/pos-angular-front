3d014d859cdc34653568f5d794b66067485fb234
education-bootcamp <education.seekerscloud@gmail.com>
2026-06-09 20:43:26 +0530

    dwdewe

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,6 +1,17 @@
 import { CanActivateFn, Router } from '@angular/router';
-
+import { inject } from '@angular/core';
+import { SnackbarService } from '../services/snackbar-service';
+import { CookieManagerService } from '../services/cookie-manager-service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  let router = inject(Router);
+  let snackbarService = inject(SnackbarService);
+  let cookieManagerService = inject(CookieManagerService);
+  if (!cookieManagerService.isLogged()) {
+    snackbarService.openSnackBar('you will have to signup with this system!', 'Close');
+    router.navigateByUrl('/home/login');
+    return false;
+  } else {
+    return true;
+  }
 };
